4659e2ad0e3c8dcc9abb885bc77e2f564ff3f990
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2020-07-15 00:35:39 +0800

    Update 1.1 Get product of all other elements.py

--- Data Structures/Arrays/1.1 Get product of all other elements.py
+++ Data Structures/Arrays/1.1 Get product of all other elements.py
@@ -9,4 +9,55 @@
 
 #Follow up: what if you can't use division?
 
+#method 1:
+def products(nums):
+    new_array = []
+    for i in range(len(nums)):
+        new_nums = nums[:i] + nums[i+1:]
+        result = 1
+        for i in range(len(new_nums)):
+            result = result * new_nums[i]
+        new_array.append(result)    
+        
+    return new_array
+ 
+    
+print(products([1,2,3,4,7,8]))  
+
+#method 2:
+def products(nums):
+    
+  prefix_products = []
+  for num in nums:
+    if prefix_products:
+      prefix_products.append(prefix_products[-1] * num)
+    else:
+      prefix_products.append(num)
+     
+    suffix_products = []
+    for num in reversed(nums):
+      if suffix_products:
+        suffix_products.append(suffix_products[-1] * num)
+      else:
+        suffix_products.append(num)
+    suffix_products = list(reversed(suffix_products))
+        
+        
+    result = []
+    for i in range(len(nums)):
+      if i == 0:
+        result.append(suffix_products[i+1])
+        
+      elif i == len(nums) - 1:
+        result.append(prefix_products[i-1])
+      
+      else:
+        result.append(prefix_products[i-1] * suffix_products[i+1])
+        
+  return result
+
+print(products([1,2,3,4,7,8])) 
+        
+        
+    
 
