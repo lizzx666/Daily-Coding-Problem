f76f9809c88b5686b1550a91944347eebbdc52bf
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2020-07-16 23:35:14 +0800

    Update 1.2 Locate smallest window to be sorted.py

--- Data Structures/Arrays/1.2 Locate smallest window to be sorted.py
+++ Data Structures/Arrays/1.2 Locate smallest window to be sorted.py
@@ -2,7 +2,7 @@
 #window that must be sorted in order for the entire array to be sorted.
 #For example, given [3,7,5,6,9], you should return (1,3)
 
-
+#method 1:
 def window(array):
     length = 0
     k = 0
@@ -10,9 +10,25 @@ def window(array):
     while k>=0 and array[k]<=array[k+1]:
         k+=1
         if k == len(array)-2:
-            return 0
+            return (1,0)
     while array[k]<array[l]:
         l-=1
     return (k,l-k+1)
 
 window([2,3,4,7,5,2,8,6])  
+
+def window(array):
+    length = 0
+    k = 0
+    for i in range(0,len(array)-1):
+        if (array[i] > array[i+1]):
+            k = i+1
+            length = 1
+            while array[k] < array[i]:
+                length += 1
+                k+=1
+                if k == len(array)-2:
+                    return (i,len(array)-i)
+    return (i,length)
+
+window([2,3,4])  
