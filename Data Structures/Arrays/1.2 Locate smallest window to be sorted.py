971bf63e40334d75053b4f46a7e40947cf85277e
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2020-07-17 00:23:08 +0800

    Update 1.2 Locate smallest window to be sorted.py

--- Data Structures/Arrays/1.2 Locate smallest window to be sorted.py
+++ Data Structures/Arrays/1.2 Locate smallest window to be sorted.py
@@ -4,6 +4,45 @@
 
 #method 1:
 def window(array):
+    left, right = None, None
+    s = sorted(array)
+    
+    for i in range(len(array)):
+        if array[i] != s[i] and left is None:
+            left = i
+        elif array[i] != s[i]:
+            right = i
+            
+    return (left,right)
+
+window([2,3,4,7,6,5,9,8])  
+
+#method 2:
+
+def window(array):
+    left, right = None, None
+    n = len(array)
+    max_seen, min_seen = - float("inf"), float("inf")
+    
+    for i in range(n):
+        max_seen = max(max_seen, array[i])
+        if array[i] < max_seen:
+            right = i
+            
+    for i in range(n-1, -1, -1):
+        min_seen = min(min_seen, array[i])
+        if array[i] > min_seen:
+            left = i
+            
+    return(left, right)
+
+window([2,3,4,7,6,5,9])  
+
+
+
+##extra: if require to return (start, window length)
+#method 1:
+def window(array):
     length = 0
     k = 0
     l = len(array) - 1
