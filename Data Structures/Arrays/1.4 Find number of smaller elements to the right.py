2ab6cca4a8f4d280677e1260fc86d405efc8d24a
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2020-08-10 00:44:24 +0800

    Update 1.4 Find number of smaller elements to the right.py

--- Data Structures/Arrays/1.4 Find number of smaller elements to the right.py
+++ Data Structures/Arrays/1.4 Find number of smaller elements to the right.py
@@ -26,3 +26,17 @@ def smaller_counts_naive(lst):
         result.append(count)
         
     return result   
+
+
+#method 2:
+def smaller_counts(lst):
+    result = []
+    seen = []
+    
+    for num in reversed(lst):
+        i = bisect.bisect_left(seen,num)
+        result.append(i)
+        bisect.insort(seen,num)
+        
+    return list(reversed(result)) 
+
