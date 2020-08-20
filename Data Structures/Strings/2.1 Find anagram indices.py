cf4543d010bb227c1f4fcbde7587988c897490b1
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2020-08-21 00:08:44 +0800

    Update 2.1 Find anagram indices.py

--- Data Structures/Strings/2.1 Find anagram indices.py
+++ Data Structures/Strings/2.1 Find anagram indices.py
@@ -2,3 +2,29 @@
 #of anagrams of w. For example, given w is ab and s is abxaba, return [0,3,4].
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+#method 1:
+from collections import Counter
+def is_anagram(s1,s2):
+    return Counter(s1) == Counter(s2)
+
+def anagram_indices(word,s):
+    result = []
+    for i in range(len(s) - len(word) + 1):
+        window = s[i:i+len(word)]
+        if is_anagram(window, word):
+            result.append(i)
+    return result
+
+
