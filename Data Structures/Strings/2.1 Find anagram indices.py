a489c5dbbfe56bd46c7ed401f5d7ab7f5e3b6906
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2020-08-23 01:07:01 +0800

    Update 2.1 Find anagram indices.py

--- Data Structures/Strings/2.1 Find anagram indices.py
+++ Data Structures/Strings/2.1 Find anagram indices.py
@@ -27,4 +27,41 @@ def anagram_indices(word,s):
             result.append(i)
     return result
 
+#method 2:
 
+#defaultdict(default_factory)
+#default_factory: A function returning the default value for the dictionary defined. If this argument is absent then the dictionary raises a KeyError.
+
+from collections import defaultdict
+def del_if_zero(dict,char):
+    if dict[char]==0:
+        del dict[char]
+        
+def anagram_indices(word,s):
+    result = []
+    
+    freq = defaultdict(int)
+    for char in word:
+        freq[char] += 1
+        
+    for char in s[:len(word)]:
+        freq[char] -= 1
+        del_if_zero(freq,char)
+    
+    if not freq:
+        result.append(0)
+        
+    for i in range(len(word),len(s)):
+        start_char, end_char = s[i-len(word)], s[i]
+        freq[start_char] += 1
+        del_if_zero(freq, start_char)
+        
+        freq[end_char] -= 1
+        del_if_zero(freq, end_char)
+        
+        if not freq:
+            beginning_index = i - len(word) + 1
+            result.append(beginning_index)
+            
+    return result
+        
